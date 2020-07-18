739c9eb92325853e9459192b7736421bd833ce67
notabucketofspam <not.a.bucket.of.spam@gmail.com>
2020-07-18 14:46:24 -0400

    Create general_all.h

---
+++ include/general_component/general_all.h
@@ -0,0 +1,18 @@
+#ifndef GENERAL_ALL_H
+#define GENERAL_ALL_H
+
+#include "general_include.h"
+#include "general_storage.h"
+#include "general_network.h"
+#include "general_server.h"
+#include "general_control.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // GENERAL_ALL_H
