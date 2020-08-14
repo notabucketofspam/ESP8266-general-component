98968b8e734e599b886c9ea5f76377f5db09d92b
notabucketofspam <not.a.bucket.of.spam@gmail.com>
2020-08-14 05:40:38 -0400

    Update general_control.c

--- general_component/general_control.c
+++ general_component/general_control.c
@@ -16,24 +16,20 @@ esp_err_t setup_control(control_config_t control_config) {
   };
   control_output.pin_bit_mask &= ~(control_config.keep_peripheral);
   control_input.pin_bit_mask &= ~(control_config.keep_peripheral);
-  ESP_LOGI(CONTROL_TAG, "pin bit mask out: %x", control_output.pin_bit_mask);
-  ESP_LOGI(CONTROL_TAG, "pin bit mask in: %x", control_input.pin_bit_mask);
+  ESP_LOGD(CONTROL_TAG, "pin bit mask out: %x", control_output.pin_bit_mask);
+  ESP_LOGD(CONTROL_TAG, "pin bit mask in: %x", control_input.pin_bit_mask);
   if (control_output.pin_bit_mask)
     ESP_ERROR_CHECK(gpio_config(&control_output));
   if (control_input.pin_bit_mask)
     ESP_ERROR_CHECK(gpio_config(&control_input));
-  ESP_LOGI(CONTROL_TAG, "C0");
   memset(s_pin_mode, 0x30, sizeof(s_pin_mode));
   memset(s_pin_bit_mask, 0x30, sizeof(s_pin_bit_mask));
   uint8_t config_index;
   for (config_index = 0; config_index < GPIO_PIN_COUNT; ++config_index) {
-    ESP_LOGI(CONTROL_TAG, "%d", config_index);
     if (!(PIN_BIT_MASK & BIT(config_index)))
       continue;
-    ESP_LOGI(CONTROL_TAG, "C1");
     s_pin_mode[config_index] = (BIT(config_index) & control_input.pin_bit_mask) ? '1' : 
       (BIT(config_index) & control_output.pin_bit_mask) ? '2' : '0';
-    ESP_LOGI(CONTROL_TAG, "C2");
     s_pin_bit_mask[config_index] = (PIN_BIT_MASK & BIT(config_index)) ? '1' : '0';
   }
   if (control_config.auto_load_persistent_pin_state)
@@ -46,6 +42,7 @@ esp_err_t load_persistent_gpio_state(void) {
   bool pin_state;
   uint8_t state_index;
   for (state_index = 0; state_index < GPIO_PIN_COUNT; ++state_index) {
+    ESP_LOGI(CONTROL_TAG, "C%d", state_index);
     pin_state = (fgetc(f_gpio_state) == '1');
     s_gpio_state_mem[state_index] = pin_state;
     if (s_pin_mode[state_index] == '2')
