0cc2d167a72273a76ef9871fc3dc3b3b0e731247
notabucketofspam <not.a.bucket.of.spam@gmail.com>
2020-08-15 13:02:54 -0400

    Update general_server.c

--- general_component/general_server.c
+++ general_component/general_server.c
@@ -20,11 +20,11 @@ httpd_uri_t base_path_get = {
 
 esp_err_t setup_server(server_config_t server_config) {
   ESP_LOGI(SERVER_TAG, "Setup server");
-  s_httpd_server = NULL;
-  server_config.httpd_handle = &s_httpd_server;
-  ESP_ERROR_CHECK(httpd_start(&s_httpd_server, &server_config.httpd_config));
-  ESP_ERROR_CHECK(httpd_register_uri_handler(s_httpd_server, &index_html_get));
-  ESP_ERROR_CHECK(httpd_register_uri_handler(s_httpd_server, &base_path_get));
+  //s_httpd_server = NULL;
+  server_config.httpd_handle = NULL;
+  ESP_ERROR_CHECK(httpd_start(&server_config.httpd_handle, &server_config.httpd_config));
+  ESP_ERROR_CHECK(httpd_register_uri_handler(server_config.httpd_handle, &index_html_get));
+  ESP_ERROR_CHECK(httpd_register_uri_handler(server_config.httpd_handle, &base_path_get));
   ESP_LOGI(SERVER_TAG, "Server OK");
   return ESP_OK;
 }
