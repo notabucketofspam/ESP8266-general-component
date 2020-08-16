4cdb45eb6d9d48607164f17031fccf2854e99a19
notabucketofspam <not.a.bucket.of.spam@gmail.com>
2020-08-16 15:18:23 -0400

    Debug info for pin mask and mode

--- general_component/general_control.c
+++ general_component/general_control.c
@@ -30,8 +30,14 @@ esp_err_t setup_control(control_config_t control_config) {
       continue;
     s_pin_mode[config_index] = (BIT(config_index) & control_input.pin_bit_mask) ? '1' : 
       (BIT(config_index) & control_output.pin_bit_mask) ? '2' : '0';
-    s_pin_bit_mask[config_index] = (PIN_BIT_MASK & BIT(config_index)) ? '1' : '0';
+    s_pin_bit_mask[config_index] = ((control_config.pin_mask_output | control_config.pin_mask_input) & 
+      BIT(config_index)) ? '1' : '0';
   }
+  char buf[GPIO_PIN_COUNT + 1] = { 0 };
+  memcpy(buf, s_pin_bit_mask, sizeof(s_pin_bit_mask));
+  ESP_LOGW(CONTROL_TAG, "%s", buf);
+  memcpy(buf, s_pin_mode, sizeof(s_pin_mode));
+  ESP_LOGW(CONTROL_TAG, "%s", buf);
   ESP_LOGI(CONTROL_TAG, "Control OK");
   return ESP_OK;
 }
